2b2d30f87f0b7982b008d4e047cb85febec80e9c
Samsky72 <samsky72@gmail.com>
2023-04-29 10:32:36 +0500

    Alias ncg was updated

--- packages/zsh.nix
+++ packages/zsh.nix
@@ -23,7 +23,7 @@ in {
     shellAliases = {
       ll = "ls -lah";
       ytd = "yt-dlp -f \"(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)\" ";
-      ncg = "sudo nix-collect-garbage -d && sudo nix store optimise";
+      ncg = "nix-collect-garbage -d && sudo nix-collect-garbage -d && sudo nix store optimise";
       update = "cd ~/NixOS && cp flake.lock flake.lock.$(date -Idate).bak && nix flake update && sudo nixos-rebuild switch --flake '.#'";
       rebuild = "cd ~/NixOS && sudo nixos-rebuild switch --flake '.#'";
     };
