2dfba5a8a5d4bfa10caf6019552ad8dc49431771
Samsky72 <samsky72@gmail.com>
2024-08-09 16:29:44 +0500

    NixVim package added

--- packages/nixvim.nix
+++ packages/nixvim.nix
@@ -1,4 +1,65 @@
-# nixVim configurations
-{ config, ... }: {
-  programs.nixvim.enable = true;
+# NixVim configurations
+{ config, userName, ... }: {
+
+  # NixVim home-manager configuration.
+  home-manager.users.${userName}.programs.nixvim = {
+    
+    enable = true;
+
+    #NixVim options.
+    opts = {
+      expandtab = true;                                                                     # Replace Tab to spaces.
+      number = true;                                                                        # Enable numbers.
+      shiftwidth = 2;                                                                       # Shift width 2 symbols.
+      smartindent = true;
+      tabstop = 2;
+    };
+
+    plugins = {
+
+      # Complete plugins.
+      cmp = {
+        autoEnableSources = true;
+        enable = true;
+        settings = {
+          mapping = {
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-j>" = "cmp.mapping.select_next_item()";
+            "<C-k>" = "cmp.mapping.select_prev_item()";
+          };
+          sources = [
+            { name = "buffer"; }
+            { name = "luasnip"; }
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+          ];
+        };
+      };
+
+      # Indent line plugins.
+      indent-blankline.enable = true;
+
+      # LSP servers plugins.
+      lsp = {
+        enable = true;
+        servers = {
+          nixd.enable = true;
+        };
+      };
+
+      # LUA snip plugins.
+      luasnip.enable= true;
+
+      # Auto pairs plugins.
+      nvim-autopairs.enable = true;
+    };
+  };
+
+  # NixVim configuration.
+  programs.nixvim = {
+    enable = true;
+  };
 }
+
