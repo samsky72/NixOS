df798746d1fd8f1ae79649890195c4e73910785c
Samsky72 <samsky72@gmail.com>
2024-08-11 18:57:14 +0500

    Some formatting

--- configs/games.nix
+++ configs/games.nix
@@ -1,7 +1,7 @@
 # Some gaming configurations.
 { config, pkgs, ... }: {
 
-  # Some gameing related softwares.
+  # Some gaming related softwares.
   environment.systemPackages = with pkgs; [
     mangohud                                              # MangoHud - Real-time game statistics (FPS, CPU/GPU loading statistics, etc.)
     retroarchFull                                         # RetroArch - Multi-platform emulator frontend for libretro cores.
@@ -9,7 +9,7 @@
    
   # Add 32 bit graphical libraries.
   hardware.graphics.enable32Bit = true;
-
+  
   # Enable steam.
   programs.steam.enable = true;
 }
