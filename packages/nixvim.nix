573e24a05417598b8144fadb9e95dfe9e8435fae
Samsky72 <samsky72@gmail.com>
2024-08-11 18:17:29 +0500

    Nixvim reconfigured for full compatibility with nixd

--- packages/nixvim.nix
+++ packages/nixvim.nix
@@ -3,11 +3,12 @@
 
   # NixVim home-manager configuration.
   home-manager.users.${userName}.programs.nixvim = {
-    
+#  programs.nixvim = {   
     enable = true;
 
     #NixVim options.
     opts = {
+      cursorline = true;
       expandtab = true;                                                                     # Replace Tab to spaces.
       number = true;                                                                        # Enable numbers.
       shiftwidth = 2;                                                                       # Shift width 2 symbols.
@@ -24,19 +25,24 @@
         settings = {
           mapping = {
             "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
             "<CR>" = "cmp.mapping.confirm({ select = true })";
             "<C-Space>" = "cmp.mapping.complete()";
             "<C-j>" = "cmp.mapping.select_next_item()";
             "<C-k>" = "cmp.mapping.select_prev_item()";
-          };
+          };   
+
           sources = [
             { name = "buffer"; }
             { name = "luasnip"; }
             { name = "nvim_lsp"; }
+            { name = "nvim_lua"; }
             { name = "path"; }
           ];
         };
       };
+      
+      cmp-treesitter.enable = true;
 
       # Indent line plugins.
       indent-blankline.enable = true;
@@ -45,19 +51,43 @@
       lsp = {
         enable = true;
         servers = {
-          nixd.enable = true;
+          nixd = {
+            enable = true;
+            settings = {
+              nixpkgs = { 
+                expr = "import <nixpkgs> { }";
+              };
+              options = {
+                flake_parts = {
+                  expr = ''let flake = builtins.getFlake ("/home/samsky/NixOS"); in flake.debug.options // flake.currentSystem.options'';
+                };
+                home_manager = {
+                  expr = ''(builtins.getFlake "/home/samsky/NixOS").homeConfigurations.${userName}.options'';
+                };
+                nixos = {
+                  expr = ''(builtins.getFlake "/home/samsky/NixOS").nixosConfigurations.hostname.options'';
+                };
+              };
+            };
+          };
         };
       };
 
       # LUA snip plugins.
-      luasnip.enable= true;
+      luasnip = {
+        enable= true;
+        fromVscode = [ ];
+      };
 
       # Auto pairs plugins.
       nvim-autopairs.enable = true;
+
+      # Treesitter plugins.
+      treesitter.enable = true;
     };
   };
 
-  # NixVim configuration.
+  #NixVim configuration.
   programs.nixvim = {
     enable = true;
   };
