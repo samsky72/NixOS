50465d41fc500095e5e9899c2b2207193c02130a
Samsky72 <samsky72@gmail.com>
2024-08-12 17:03:43 +0500

    Formatting of virtualisation.nix

--- configs/virtualisation.nix
+++ configs/virtualisation.nix
@@ -5,20 +5,24 @@
     qtemu
   ];
   
-  # Libvirt configuration.
-  virtualisation.libvirtd = {
-    enable = true;
+  # Virtualisation configuration.
+  virtualisation = {
+    
+    # Libvirt configuration.
+    libvirtd = {
+      enable = true;
       qemu = {
-      package = pkgs.qemu_full;         # Full QEMU in use.
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [(pkgs.OVMF.override { 
-          secureBoot = true;
-          tpmSupport = true;
-        }).fd ];
+        package = pkgs.qemu_full;         # Full QEMU in use.
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [(pkgs.OVMF.override { 
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd ];
+        };
       };
     };
-  };  
+  };
 }
