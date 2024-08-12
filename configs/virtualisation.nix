040f1c7d37bdc95b5352dfe653cc8dea9375aae1
Samsky72 <samsky72@gmail.com>
2024-08-12 13:44:55 +0500

    The libvirt configured

--- configs/virtualisation.nix
+++ configs/virtualisation.nix
@@ -1,11 +1,24 @@
 # Virtualisation configuration.
 { config, pkgs, ... }: {
 
+  environment.systemPackages = with pkgs; [
+    qtemu
+  ];
+  
   # Libvirt configuration.
   virtualisation.libvirtd = {
     enable = true;
       qemu = {
-      package = pkgs.qemu_full;         # Full QEMU in use. 
+      package = pkgs.qemu_full;         # Full QEMU in use.
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override { 
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd ];
+      };
     };
   };  
 }
