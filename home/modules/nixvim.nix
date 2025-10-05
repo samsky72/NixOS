cd36b4a5a0c350ff890dee9018d7980f2c3175e9
samsky <samsky72@gmail.com>
2025-10-05 21:05:38 +0500

    Nixvim reconfigured

--- home/modules/nixvim.nix
+++ home/modules/nixvim.nix
@@ -1,9 +1,5 @@
-# home/modules/nixvim.nix
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 {
-  # Import nixvim's Home Manager module
-  imports = [ inputs.nixvim.homeModules.nixvim ];
-
   programs.nixvim = {
     enable = true;
     viAlias = true;
@@ -12,125 +8,148 @@
     ########################################
     ## Look & feel
     ########################################
-    colorschemes.catppuccin = {
-      enable = true;
-      flavour = "mocha";
-    };
+    colorschemes.tokyonight.enable = true;
 
     opts = {
       number = true;
       relativenumber = true;
-      mouse = "a";
+      tabstop = 2;
+      shiftwidth = 2;
+      expandtab = true;
+      smartindent = true;
+      wrap = false;
+      cursorline = true;
       termguicolors = true;
       signcolumn = "yes";
-      shiftwidth = 2;
-      tabstop = 2;
-      expandtab = true;
-      updatetime = 300;
-      timeoutlen = 500;
+      updatetime = 250;
+      timeoutlen = 400;
     };
+
+    ########################################
+    ## Keymaps (no desc)
+    ########################################
+    keymaps = [
+      { mode = "n"; key = "<leader>ff"; action = "<cmd>Telescope find_files<CR>"; }
+      { mode = "n"; key = "<leader>fg"; action = "<cmd>Telescope live_grep<CR>"; }
+      { mode = "n"; key = "<leader>fb"; action = "<cmd>Telescope buffers<CR>"; }
+      { mode = "n"; key = "<leader>fh"; action = "<cmd>Telescope help_tags<CR>"; }
+      { mode = "n"; key = "<leader>e";  action = "<cmd>NvimTreeToggle<CR>"; }
+
+      { mode = "n"; key = "gd";         action = "<cmd>lua vim.lsp.buf.definition()<CR>"; }
+      { mode = "n"; key = "gr";         action = "<cmd>Telescope lsp_references<CR>"; }
+      { mode = "n"; key = "K";          action = "<cmd>lua vim.lsp.buf.hover()<CR>"; }
+      { mode = "n"; key = "<leader>rn"; action = "<cmd>lua vim.lsp.buf.rename()<CR>"; }
+      { mode = "n"; key = "<leader>ca"; action = "<cmd>lua vim.lsp.buf.code_action()<CR>"; }
+      { mode = "n"; key = "<leader>f";  action = "<cmd>lua require('conform').format({ async = true })<CR>"; }
+    ];
 
     ########################################
     ## Plugins
     ########################################
     plugins = {
-      # UI
-      lualine.enable = true;
-      which-key.enable = true;
-      telescope.enable = true;
+      # Explicit devicons (silences the deprecation warning)
+      web-devicons.enable = true;
 
-      # Treesitter
+      lualine.enable = true;
+      telescope.enable = true;
+      nvim-tree.enable = true;
+      nvim-autopairs.enable = true;
+
       treesitter = {
         enable = true;
-        indent = true;
-        folding = true;
+        settings = {
+          ensure_installed = [ "nix" "lua" "python" "rust" "bash" "markdown" "json" "yaml" "tsx" "html" "css" ];
+          indent.enable = true;
+          highlight.enable = true;
+        };
       };
 
-      # LSP setup
+      # LSP
       lsp = {
         enable = true;
         servers = {
-          nixd.enable = true;        # ✅ replaces nil_ls
+          nixd.enable = true;
           lua_ls.enable = true;
           pyright.enable = true;
-          tsserver.enable = true;
+          ts_ls.enable = true;          # <- renamed from tsserver/ts-ls
           bashls.enable = true;
           html.enable = true;
           cssls.enable = true;
           jsonls.enable = true;
           yamlls.enable = true;
+
+          rust_analyzer = {
+            enable = true;
+            # Silence cargo/rustc warnings by letting Nixvim provide them:
+            installCargo = true;
+            installRustc = true;
+            # (Optional) choose specific packages:
+            # cargoPackage = pkgs.rust-bin.stable.latest.default;   # if you use rust-overlay
+            # rustcPackage  = pkgs.rustc;
+          };
         };
       };
 
-      # Completion
+      # Completion (nvim-cmp)
       cmp = {
         enable = true;
-        autoEnableSources = true;
-        sources = [
-          { name = "nvim_lsp"; }
-          { name = "buffer"; }
-          { name = "path"; }
-          { name = "luasnip"; }
-        ];
+        settings = {
+          snippet = { expand = "luasnip"; };
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "buffer"; }
+            { name = "path"; }
+            { name = "luasnip"; }
+          ];
+          mapping = {
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<CR>"      = "cmp.mapping.confirm({ select = true })";
+            "<Tab>"     = "cmp.mapping.select_next_item()";
+            "<S-Tab>"   = "cmp.mapping.select_prev_item()";
+            "<C-b>"     = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>"     = "cmp.mapping.scroll_docs(4)";
+            "<C-e>"     = "cmp.mapping.close()";
+          };
+        };
       };
+
       luasnip.enable = true;
 
-      # Formatting
+      # Formatting (conform.nvim)
       conform-nvim = {
         enable = true;
         settings = {
           formatters_by_ft = {
             nix = [ "nixpkgs-fmt" ];
             lua = [ "stylua" ];
-            python = [ "ruff_format" "ruff_organize_imports" ];
+            python = [ "black" ];              # or ruff_format
+            rust = [ "rustfmt" ];
+            sh = [ "shfmt" ];
             javascript = [ "prettierd" "prettier" ];
             typescript = [ "prettierd" "prettier" ];
             json = [ "prettierd" "prettier" ];
             yaml = [ "prettierd" "prettier" ];
             markdown = [ "prettierd" "prettier" ];
-            sh = [ "shfmt" ];
           };
           format_on_save = { lspFallback = true; timeoutMs = 1500; };
         };
       };
-
-      gitsigns.enable = true;
-      noice.enable = true;
-      notify.enable = true;
     };
 
     ########################################
-    ## Keymaps
-    ########################################
-    globals.mapleader = " ";
-    keymaps = [
-      { mode = "n"; key = "<leader>ff"; action = "<cmd>Telescope find_files<CR>"; desc = "Find files"; }
-      { mode = "n"; key = "<leader>fg"; action = "<cmd>Telescope live_grep<CR>";  desc = "Live grep"; }
-      { mode = "n"; key = "<leader>fb"; action = "<cmd>Telescope buffers<CR>";    desc = "Buffers"; }
-      { mode = "n"; key = "<leader>fh"; action = "<cmd>Telescope help_tags<CR>";  desc = "Help"; }
-
-      { mode = "n"; key = "gd"; action = "<cmd>lua vim.lsp.buf.definition()<CR>"; desc = "Goto def"; }
-      { mode = "n"; key = "gr"; action = "<cmd>Telescope lsp_references<CR>"; desc = "References"; }
-      { mode = "n"; key = "K";  action = "<cmd>lua vim.lsp.buf.hover()<CR>"; desc = "Hover"; }
-      { mode = "n"; key = "<leader>rn"; action = "<cmd>lua vim.lsp.buf.rename()<CR>"; desc = "Rename"; }
-      { mode = "n"; key = "<leader>ca"; action = "<cmd>lua vim.lsp.buf.code_action()<CR>"; desc = "Code action"; }
-      { mode = "n"; key = "<leader>f"; action = "<cmd>lua require('conform').format({ async = true })<CR>"; desc = "Format"; }
-    ];
-
-    ########################################
-    ## External tools
+    ## Extra tools Neovim relies on
     ########################################
     extraPackages = with pkgs; [
       nixd
       nixpkgs-fmt
       stylua
-      ruff
-      ruff-lsp
-      nodePackages.prettier
-      prettierd
+      black
+      rustfmt
       shfmt
       ripgrep
       fd
+      nodePackages.prettier
+      prettierd
     ];
   };
 }
